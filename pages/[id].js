fc4250289eaa07b3b853f3ca2aefad2bd5c3ab7e
Jeff Passetti <jpassetti@Jeffs-MacBook-Air.local>
2023-02-22 13:13:34 -0500

    about us

--- pages/[id].js
+++ pages/[id].js
@@ -1,11 +1,16 @@
 import Head from 'next/head'
 
+import Col from '../components/Col'
 import Container from '../components/Container'
 import Heading from '../components/Heading'
+import Image from 'next/image'
 import Layout from '../components/Layout'
 import MainContent from "../components/MainContent"
+import Paragraph from '../components/Paragraph'
+import Row from '../components/Row'
+import Section from '../components/Section'
 
-import { getAllPageSlugs, getPageBySlug, getMainMenuItems } from '../lib/api'
+import { getAllPageSlugs, getPageBySlug, getMainMenuItems, getProjectTypes } from '../lib/api'
 
 export async function getStaticPaths() {
 
@@ -29,17 +34,18 @@ export async function getStaticProps({ params }) {
 
 	const pageData = await getPageBySlug(params.id);
 	const menuItems = await getMainMenuItems();
+	const projectTypes = await getProjectTypes();
 
 	return {
 		props: {
-			pageData, menuItems
+			pageData, menuItems, projectTypes
 		}
 	}
 
 }
 
-const BasicPageTemplate = ({pageData, menuItems}) => {
-    const { title, content } = pageData;
+const BasicPageTemplate = ({pageData, menuItems, projectTypes}) => {
+    const { title, slug, content } = pageData;
     return <Layout menuItems={menuItems}>
 		<Head>
 			<title>{title} | Empire Housing and Development Corporation | Syracuse, NY</title>
@@ -47,6 +53,38 @@ const BasicPageTemplate = ({pageData, menuItems}) => {
 		<Container>
         <Heading name="h1">{title}</Heading>
         <MainContent content={content} />
+		{slug === "about-us" && projectTypes.map(edge => {
+                const {node} = edge;
+                const {name, staffMembers} = node;
+                return  <Section>
+                    <Heading name="h2">{name}</Heading>
+                    <Row>
+                        {staffMembers.edges.map(edge => {
+                            const {node} = edge;
+                            const {title, featuredImage, personInformation} = node;
+                            const {position} = personInformation;
+                            return <Col xs="12" md="6" marginBottom="1">
+                                <Row>
+                                    
+                                {featuredImage &&
+                                    <Col xs="4" sm="4"><Image
+                                        src={featuredImage.node.sourceUrl}
+                                        alt={featuredImage.node.altText}
+                                        width={featuredImage.node.mediaDetails.width}
+                                        height={featuredImage.node.mediaDetails.height}
+                                        layout="responsive"
+                                    /></Col>
+                                }
+                                <Col xs="8" sm="8" flexDirection="column" justifyContent="center">
+                                <Heading name="h3" marginBottom="0">{title}</Heading>
+                                {position && <Paragraph>{position}</Paragraph>}
+                                </Col>
+                                </Row>
+                            </Col>
+                        })}
+                    </Row>
+                </Section>
+            })}
 		</Container>
     </Layout>
 }
